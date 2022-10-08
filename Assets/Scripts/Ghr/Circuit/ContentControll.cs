fe7720808168acbb1e09a993041c522f2f6ad83c
GGhr <1102914987>
2022-10-08 22:42:25 +0800

    #简单实现电路解谜、qte代码，工厂场景搭建

---
+++ Assets/Scripts/Ghr/Circuit/ContentControll.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class ContentControll : MonoBehaviour
+{
+    public static ContentControll instance;
+    public List<ItemsControll> itemsList= new List<ItemsControll>();
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        init();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            itemsList.Add(transform.GetChild(i).GetComponent<ItemsControll>());
+        }
+    }
+    //首先进行初始化
+    void init()
+    {
+        itemsList.Clear();
+    }
+    /// <summary>
+    /// 检查子物体是否全部为正确状态
+    /// </summary>
+    public void CheckItems()
+    {
+        bool isAllTrue=false;
+        for(int i=0;i<itemsList.Count;i++)
+        {
+            var item = itemsList[i];
+            if (item.isTrue != true)
+                break;
+            if(item.isTrue==true&&i==itemsList.Count-1)
+                isAllTrue=true;
+        }
+        if (isAllTrue)
+        {
+            Debug.Log("全部解锁进行下一步操作");
+        }
+    }
+}
---
+++ Assets/Scripts/Ghr/Circuit/ItemsControll.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 方向类型，上下左右
+/// </summary>
+public enum DirectionType
+{
+    up,
+    down,
+    left,
+    right
+}
+public class ItemsControll : MonoBehaviour
+{
+    public DirectionType myDir=DirectionType.up;//目前的方向类型
+    public DirectionType trueDir;//正确的方向
+    public bool isTrue=false;//是否正确
+    /// <summary>
+    /// 当鼠标点击时进行判断
+    /// </summary>
+    private void OnMouseDown()
+    {
+        switch(myDir)
+        {
+            case DirectionType.up:
+                myDir = DirectionType.right;
+                gameObject.transform.Rotate(new Vector3(0, 0, -90f));//设置方向，并进行旋转
+                if (myDir == trueDir)
+                    isTrue = true;
+                else
+                    isTrue = false;//判断是否为正确方向，是则为true否则为false
+                Debug.Log("目前" + myDir + " " + isTrue);
+                ContentControll.instance.CheckItems();//调用检查脚本，遍历子物体进行判断
+                break;
+            case DirectionType.right:
+                myDir = DirectionType.down;
+                gameObject.transform.Rotate(new Vector3(0, 0, -90f));
+                if (myDir == trueDir)
+                    isTrue = true;
+                else
+                    isTrue = false;
+                Debug.Log("目前" + myDir + " " + isTrue);
+                ContentControll.instance.CheckItems();
+                break;
+            case DirectionType.down:
+                myDir = DirectionType.left;
+                gameObject.transform.Rotate(new Vector3(0, 0, -90f));
+                if (myDir == trueDir)
+                    isTrue = true;
+                else
+                    isTrue = false;
+                Debug.Log("目前" + myDir + " " + isTrue);
+                ContentControll.instance.CheckItems();
+                break;
+            case DirectionType.left:
+                myDir = DirectionType.up;
+                gameObject.transform.Rotate(new Vector3(0, 0, -90f));
+                if (myDir == trueDir)
+                    isTrue = true;
+                else
+                    isTrue = false;
+                Debug.Log("目前" + myDir + " " + isTrue);
+                ContentControll.instance.CheckItems();
+                break;
+           
+
+        }
+    }
+}
