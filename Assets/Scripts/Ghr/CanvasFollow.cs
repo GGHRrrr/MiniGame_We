756065fe14264477b0a1a127c02445d9facaece7
GGhr <1102914987>
2022-10-12 22:24:30 +0800

    #分别跟随

--- Assets/Scripts/Ghr/CanvasFollow.cs
+++ Assets/Scripts/Ghr/CanvasFollow.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class CanvasFollow : MonoBehaviour
 {
-    private Transform playerTarget;
+    public Transform followTarget;
     public float smoothSpeed;
-    private void Start()
-    {
-        playerTarget = GameObject.Find("Player/Human").transform;
-    }
+
     void Update()
     {
-        if (playerTarget.transform.position.x != transform.position.x)
+        if (followTarget.transform.position.x != transform.position.x)
         {
             Vector3 pos;
-            pos = new Vector3(playerTarget.position.x, transform.position.y,transform.position.z);
+            pos = new Vector3(followTarget.position.x, transform.position.y,transform.position.z);
             transform.position = Vector3.Lerp(transform.position, pos, smoothSpeed);
         }
     }
