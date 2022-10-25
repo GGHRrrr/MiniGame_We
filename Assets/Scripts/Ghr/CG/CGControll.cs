a89de9bcdcf447489b26cba35d87b07a7df7490a
GGhr <1102914987>
2022-10-26 00:57:00 +0800

    #开头CG

---
+++ Assets/Scripts/Ghr/CG/CGControll.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CGControll : MonoBehaviour
+{
+    public Animator black;
+    public GameObject d;
+    public bool isBlackTrue=false;
+    public GameObject player;
+    public GameObject ca;
+    private void Start()
+    {
+        StartCoroutine(CGcontroll());
+    }
+
+    private void Update()
+    {
+        if(isBlackTrue&& Input.GetKeyDown(KeyCode.D))
+        {
+            d.gameObject.SetActive(false);
+            StartCoroutine(PlayerControll());
+        }
+    }
+    IEnumerator CGcontroll()
+    {
+        yield return null;
+        AnimatorStateInfo stateinfo= black.GetCurrentAnimatorStateInfo(0);
+        if (stateinfo.IsName("black") && (stateinfo.normalizedTime > 1.0f))
+        {
+            isBlackTrue = true;
+            black.gameObject.SetActive(false);
+        }
+            else
+        {
+            StartCoroutine(CGcontroll());
+        }
+
+    }
+    IEnumerator PlayerControll()
+    {
+        gameObject.GetComponent<Animator>().SetBool("walk", true);
+        gameObject.GetComponent<Animator>().speed = 0.8f;
+        player.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(1f);
+        ca.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+}
